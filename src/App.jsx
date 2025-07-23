904b07727ea8abe13f6108c32ab13a5ce55e3f33
minseo9 <kmsals130@gmail.com>
2025-07-23 17:41:00 +0900

    header ui 구현

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,34 @@ import AttendancePage from "./pages/AttendancePage";
 import PayrollPage from "./pages/PayrollPage";
 import RetirementPage from "./pages/RetirementPage";
 
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+
 function App() {
-    const [login, setLogin] = useState(false);
+    const [login, setLogin] = useState(true);
 
     return (
         <>
-            <Routes>
-                <Route path="/" element={<LoginPage />} />
-                <Route path="/info" element={<CompanyInfoPage />} />
-                <Route path="/employee" element={<EmployeePage />} />
-                <Route path="/attendance" element={<AttendancePage />} />
-                <Route path="/payroll" element={<PayrollPage />} />
-                <Route path="/retirement" element={<RetirementPage />} />
-            </Routes>
+            {login && <Header />}
+            <div>
+                {login && <SideBar />}
+                <main>
+                    <Routes>
+                        <Route path="/" element={<LoginPage />} />
+                        <Route path="/info" element={<CompanyInfoPage />} />
+                        <Route path="/employee" element={<EmployeePage />} />
+                        <Route
+                            path="/attendance"
+                            element={<AttendancePage />}
+                        />
+                        <Route path="/payroll" element={<PayrollPage />} />
+                        <Route
+                            path="/retirement"
+                            element={<RetirementPage />}
+                        />
+                    </Routes>
+                </main>
+            </div>
         </>
     );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import companyLogo from "../assets/company-logo.png";
+import setting from "../assets/settings-icon.png";
+import exitDoor from "../assets/exit-door-icon.png";
+
+const Header = () => {
+    return (
+        <div className="bm border-gray flex border-b">
+            <div className="flex flex-1 items-center justify-between px-5 py-2">
+                <img
+                    src={companyLogo}
+                    alt="회사 로고"
+                    className="h-[50px] w-[50px]"
+                />
+                <span className="text-xs font-bold">000님</span>
+            </div>
+            <div className="flex items-center px-3">
+                <button className="cursor-pointer">
+                    <img
+                        src={setting}
+                        alt="설정"
+                        className="h-[20px] w-[20px]"
+                    />
+                </button>
+            </div>
+            <div className="flex items-center px-5">
+                <button className="cursor-pointer">
+                    <img
+                        src={exitDoor}
+                        alt="로그아웃"
+                        className="h-[20px] w-[20px]"
+                    />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
