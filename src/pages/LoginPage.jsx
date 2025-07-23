02003bd48c4852d49c9787a8cc536081afa93981
minseo9 <kmsals130@gmail.com>
2025-07-23 17:23:23 +0900

    로그인 페이지 ui 변경

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,12 +4,12 @@ const LoginPage = () => {
             <h1 className="text-main text-center text-3xl font-bold">
                 MJ SECURITY
             </h1>
-            <div className="mt-3 rounded-md border border-solid border-gray-400 p-5">
-                <div className="flex flex-col rounded-md border border-solid border-gray-400">
+            <div className="border-gray mt-4 rounded-md border border-solid p-5">
+                <div className="border-gray flex flex-col rounded-md border border-solid">
                     <input
                         type="text"
                         placeholder="아이디"
-                        className="border-b border-gray-400 p-3 text-sm"
+                        className="border-gray border-b p-3 text-sm"
                     />
                     <input
                         type="password"
@@ -18,11 +18,20 @@ const LoginPage = () => {
                     />
                 </div>
                 <div className="my-3">
-                    <input type="checkbox" className="accent-main" />
-                    <span className="pl-2 text-sm">로그인 상태 유지</span>
+                    <input
+                        type="checkbox"
+                        id="state"
+                        className="accent-main cursor-pointer"
+                    />
+                    <label
+                        htmlFor="state"
+                        className="cursor-pointer pl-2 text-sm"
+                    >
+                        로그인 상태 유지
+                    </label>
                 </div>
                 <div>
-                    <button className="bg-main h-[40px] w-full rounded-md font-bold text-white">
+                    <button className="bg-main h-[40px] w-full cursor-pointer rounded-md font-bold text-white">
                         로그인
                     </button>
                 </div>
