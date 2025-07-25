d2acd5fcc5cdf7fd639eeab0ab6808e5a80e26cc
minseo9 <kmsals130@gmail.com>
2025-07-25 17:18:01 +0900

    부서 정보 페이지 ui 구현

--- src/pages/info/DepartmentInfoPage.jsx
+++ src/pages/info/DepartmentInfoPage.jsx
@@ -1,5 +1,17 @@
+import { useLocation } from "react-router-dom";
+
+import Title from "../../components/Title";
+import chart from "../../assets/department-chart.png";
+
 const DepartmentInfoPage = () => {
-    return <div>부서 정보</div>;
+    const location = useLocation().state.category;
+
+    return (
+        <div>
+            <Title text={location} />
+            <img src={chart} alt="부서 조직도" />
+        </div>
+    );
 };
 
 export default DepartmentInfoPage;
