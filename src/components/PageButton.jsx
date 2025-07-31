c2156e69bd47a32c57d8455e3ab70b988226143f
minseo9 <kmsals130@gmail.com>
2025-07-31 15:55:02 +0900

    페이지 버튼 컴포넌트 수정

--- src/components/PageButton.jsx
+++ src/components/PageButton.jsx
@@ -1,20 +1,22 @@
 const PageButton = ({ page, pageNumber }) => {
-    {
-        page >= 6 && <button>{"< 이전"}</button>;
-    }
-    <div className="flex gap-3">
-        {Array.from({ length: 5 }, (_, i) =>
-            pageNumber > i ? (
-                <button
-                    key={i}
-                    className={i + 1 === page ? "text-black" : null}
-                >
-                    {i + 1}
-                </button>
-            ) : null,
-        )}
-    </div>;
-    {
-        pageNumber > 5 && <button>{"다음 >"}</button>;
-    }
+    return (
+        <div>
+            {page >= 6 && <button>{"< 이전"}</button>}
+            <div className="flex gap-3">
+                {Array.from({ length: 5 }, (_, i) =>
+                    pageNumber > i ? (
+                        <button
+                            key={i}
+                            className={i + 1 === page ? "text-black" : null}
+                        >
+                            {i + 1}
+                        </button>
+                    ) : null,
+                )}
+            </div>
+            ;{pageNumber > 5 && <button>{"다음 >"}</button>}
+        </div>
+    );
 };
+
+export default PageButton;
