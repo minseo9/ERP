ed7da72c1617eef285b2b738cf128cb2ea11754b
minseo9 <kmsals130@gmail.com>
2025-08-04 14:32:42 +0900

    style: 연차 신청 페이지 표 스타일 변경

--- src/pages/attendance/AnnualLeaveFormPage.jsx
+++ src/pages/attendance/AnnualLeaveFormPage.jsx
@@ -26,38 +26,65 @@ const AnnualLeaveFormPage = () => {
                 <section>
                     <h2>연차 신청서</h2>
                 </section>
-                <section className="m-auto mt-15 w-2/3">
-                    <div className="border-default-gray mb-3 flex items-center justify-center gap-1 border">
+                <section className="border-default-gray m-auto mt-15 grid w-2/3 border">
+                    <div className="grid grid-cols-[1.5fr_3fr_1fr_4fr]">
                         <label
                             htmlFor="name"
-                            className="bg-default-gray px-3 py-2"
+                            className="bg-default-gray border-default-gray border-b py-3 text-center"
                         >
                             이름
                         </label>
-                        <input type="text" id="name" className="flex-1" />
+                        <input
+                            type="text"
+                            id="name"
+                            className="border-default-gray border-b px-3"
+                        />
                         <label
                             htmlFor="department"
-                            className="bg-default-gray px-3 py-2"
+                            className="bg-default-gray border-default-gray border-b py-3 text-center"
                         >
                             부서
                         </label>
-                        <input type="text" id="department" className="flex-1" />
+                        <input
+                            type="text"
+                            id="department"
+                            className="border-default-gray border-b px-3"
+                        />
+                        <label
+                            htmlFor="phone"
+                            className="bg-default-gray border-default-gray border-b py-3 text-center"
+                        >
+                            비상연락망
+                        </label>
+                        <input
+                            type="text"
+                            id="phone"
+                            className="border-default-gray border-b px-3"
+                        />
                         <label
                             htmlFor="position"
-                            className="bg-default-gray px-3 py-2"
+                            className="bg-default-gray border-default-gray border-b py-3 text-center"
                         >
                             직급
                         </label>
-                        <input type="text" id="position" className="flex-1" />
+                        <input
+                            type="text"
+                            id="position"
+                            className="border-default-gray border-b px-3"
+                        />
                     </div>
-                    <div className="border-default-gray mb-3 flex items-center justify-center gap-1 border">
+                    <div className="grid grid-cols-[1.5fr_3fr_1fr_4fr]">
                         <label
                             htmlFor="type"
-                            className="bg-default-gray px-3 py-2"
+                            className="bg-default-gray border-default-gray border-b py-3 text-center"
                         >
                             연차 종류
                         </label>
-                        <select name="" id="type" className="flex-1">
+                        <select
+                            name=""
+                            id="type"
+                            className="border-default-gray border-b px-3"
+                        >
                             {annualLeaveType.map((data) => (
                                 <option value={data} key={data}>
                                     {data}
@@ -65,32 +92,25 @@ const AnnualLeaveFormPage = () => {
                             ))}
                         </select>
                         <label
-                            htmlFor="phone"
-                            className="bg-default-gray px-3 py-2"
-                        >
-                            비상연락망
-                        </label>
-                        <input type="text" id="phone" className="flex-1" />
-                    </div>
-                    <div className="border-default-gray mb-3 flex items-center gap-3 border">
-                        <label
                             htmlFor="date"
-                            className="bg-default-gray px-3 py-2"
+                            className="bg-default-gray border-default-gray border-b py-3 text-center"
                         >
                             기간
                         </label>
-                        <input type="date" id="date" className="mr-2" />
-                        -
-                        <input type="date" id="date" className="ml-2" />
+                        <div className="border-default-gray flex items-center justify-center gap-4 border-b">
+                            <input type="date" id="date" className="" />
+                            -
+                            <input type="date" id="date" className="" />
+                        </div>
                     </div>
-                    <div className="border-default-gray mb-3 flex h-[80px] border">
+                    <div className="grid h-[80px] grid-cols-[1.5fr_8fr]">
                         <label
                             htmlFor=""
-                            className="bg-default-gray m-auto h-full px-3 py-6"
+                            className="bg-default-gray border-default-gray border-b py-3 text-center"
                         >
                             사유
                         </label>
-                        <textarea className="h-full flex-1 resize-none p-2"></textarea>
+                        <textarea className="h-full resize-none p-2"></textarea>
                     </div>
                 </section>
                 <section className="m-auto mt-10 w-2/3">
